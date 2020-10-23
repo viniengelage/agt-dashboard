5b21bb29f3a5893445186b82755dbfec1a175a5c
Vinicios Engelage <50602159+viniengelage@users.noreply.github.com>
2020-10-23 09:04:52 -0300

    just commit

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import {
-  Redirect,
-  Route as ReactDOMRoute,
-} from 'react-router-dom';
+import React from "react";
+import { Switch, Redirect, Route } from "react-router-dom";
+import { PrivateRoute } from "./AuthRoute";
+import AdminRoutes from "./admin";
+import UnAuthRoutes from "./normal";
 
-import Home from '../pages/Home'
-
-const IndexRoutes = ({
-  component: Component,
-  ...rest
-}) => {
-  const isLogged = !!localStorage.getItem('@AgoraTem:acess_token');
-  return isLogged ? (
-    <ReactDOMRoute
-      {...rest}
-      render={() => {
-        return <Home/>;
-      }}
-    />
-  ) : (
-    <Redirect to="/login" />
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={() => <Redirect to="/auth" />} />
+      <Route path="/auth" component={UnAuthRoutes} />
+      <PrivateRoute path="/admin" component={AdminRoutes} />
+    </Switch>
   );
 };
 
-export default IndexRoutes;
+export default Routes;
